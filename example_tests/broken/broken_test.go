8c20e40634747e031ef01844e5f368f428cb7a49
Adam Hamrick <adam.hamrick@smartcontract.com>
2025-06-20 17:11:45 -0400

    docs

--- example_tests/broken/broken_test.go
+++ example_tests/broken/broken_test.go
@@ -7,6 +7,6 @@ import "testing"
 func TestBroken(t *testing.T) {
 	t.Fatal("I shouldn't be able to compile")
 	// This test is intentionally broken and unable to compile to help us test build errors.
-	// The line below is intentionally invalid Go syntax:
+	// The line below is intentionally broken Go:
 	var v int = "not an int"
 }
